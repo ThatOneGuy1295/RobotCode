4c97f172317d1028e26173d871af63790b2d6dbc
ThatOneGuy1295 <149629123+ThatOneGuy1295@users.noreply.github.com>
2023-11-15 16:37:24 -0500

    blah

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,12 +4,25 @@ public final class Constants {
     
     public static class CANIds 
     {
-
+        public static final int leftWheelMoter = 1;
+        public static final int rightWheelMoter = 2;
     }
+
     public static class DIO 
     {
-
+        public static int WheelLimitSwitch = 1;
     }
+
+    public static class Wheels
+    {
+        public static int leftWheelMoter = CANIds.leftWheelMoter;
+        public static int rightWheelMoter = CANIds.rightWheelMoter;
+
+        public static double WheelForwardSpeed = 1;
+        public static double WheelBackwardsSpeed = -1;
+    }
+
+
 }
 
 
